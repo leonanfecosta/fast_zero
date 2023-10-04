fc4af689bd759632589b0d911064c4c7bb71626f
Leonan Tavares <leonan.tavares@dynamox.net>
2023-10-04 10:45:43 -0300

    feat: add route DELETE users/user_id for delete users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import UserDB, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -35,3 +35,13 @@ def update_user(user_id: int, user: UserSchema):
     database[user_id - 1] = user_with_id
 
     return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'detail': 'User deleted'}
